c16e2b87d0b0d034ceb8f7c234b6d7af98576a90
Vava_62600 <aideenligne62@gmail.com>
2023-10-23 18:47:36 +0200

    Login.php are modified

--- Login.php
+++ Login.php
@@ -39,7 +39,7 @@
 
         input[type="text"],
         input[type="password"] {
-            width: 100%;
+            width: 281px;
             padding: 10px;
             border: 1px solid #ccc;
             border-radius: 3px;
@@ -74,27 +74,6 @@
     </style>
 </head>
 <body>
-    <?php
-    // Exemple d'ID d'utilisateur, à remplacer par votre logique de base de données
-    $utilisateurs = [
-        'utilisateur1' => ['motdepasse' => 'MotDePasse1'],
-        'utilisateur2' => ['motdepasse' => 'MotDePasse2'],
-    ];
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $utilisateur = $_POST['utilisateur'];
-        $motdepasse = $_POST['motdepasse'];
-
-        if (array_key_exists($utilisateur, $utilisateurs) && $utilisateurs[$utilisateur]['motdepasse'] === $motdepasse) {
-            // L'utilisateur est authentifié, vous pouvez rediriger ou afficher le contenu protégé ici
-            echo '<h1>Bienvenue, ' . htmlspecialchars($utilisateur) . ' !</h1>';
-        } else {
-            // L'utilisateur n'a pas pu être authentifié, affichez un message d'erreur
-            echo '<h2>Identifiants incorrects. Veuillez réessayer.</h2>';
-        }
-    }
-    ?>
-
     <div class="container">
         <div class="login-box">
             <h2>Connexion</h2>
